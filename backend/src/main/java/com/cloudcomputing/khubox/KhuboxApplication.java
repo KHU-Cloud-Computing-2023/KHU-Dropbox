00011b3612308706b6fc21c507a0c0e0556db7cf
min1925k <min1925k@gmail.com>
2023-04-11 19:14:58 +0900

    Backend initial commit

---
+++ backend/src/main/java/com/cloudcomputing/khubox/KhuboxApplication.java
@@ -0,0 +1,13 @@
+package com.cloudcomputing.khubox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhuboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhuboxApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cloudcomputing/khubox/KhuboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudcomputing.khubox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhuboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
