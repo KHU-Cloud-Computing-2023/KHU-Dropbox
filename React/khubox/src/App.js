0735ab9f0e004d81a0fc4bb6dd93f130c8196d3b
gyulim5 <ohrbfla1004@khu.ac.kr>
2023-05-10 23:14:27 +0900

    add Project

---
+++ React/khubox/src/App.js
@@ -0,0 +1,12 @@
+import LoginPage from '../src/components/LoginPage';
+import '../src/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
