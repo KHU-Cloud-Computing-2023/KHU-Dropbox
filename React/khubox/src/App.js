ca4f22743be5c123cdab196523b4fa6d3d600787
34suuuuu <psh7492@gmail.com>
2023-06-03 19:27:39 +0900

    수정

--- React/khubox/src/App.js
+++ React/khubox/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import LogIn from './components/LogIn';
@@ -8,8 +8,18 @@ import FolderCreator from './components/FolderCreator';
 import GroupPage from './components/GroupPage';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch('/api/hello')
+        .then(response => response.text())
+        .then(message => {
+          setMessage(message);
+        });
+  },[])
   return (
     <Router>
+      <h1 className="App-title">{message}</h1>
       <Routes>
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
