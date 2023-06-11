e5f8010299f2b5a704aa37fac185a0baef320cd0
34suuuuu <psh7492@gmail.com>
2023-06-11 09:09:15 +0900

    FileUploader, FolderCreator 토글 추가

--- React/khubox/src/components/Files.js
+++ React/khubox/src/components/Files.js
@@ -1,53 +1,53 @@
-import React, {useState, useEffect} from 'react';
-import {Link, useLocation, useParams} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import NavBar from './Navbar';
 import FileUploader from './FileUploader';
 import FolderCreator from './FolderCreator';
 import '../css/Files.css';
-import {BiDotsVerticalRounded, BiFolder} from 'react-icons/bi';
+import { BiDotsVerticalRounded, BiFolder } from 'react-icons/bi';
 import { BsFileEarmark } from "react-icons/bs";
 
 function FileLists({ folders, files }) {
     return (
         <table className="table">
             <thead>
-            <tr>
-                <th scope="col"></th>
-                <th scope="col">Name</th>
-                <th scope="col">Modified</th>
-                <th scope="col">Options</th>
-            </tr>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Modified</th>
+                    <th scope="col">Options</th>
+                </tr>
             </thead>
             <tbody>
-            {/* 파일 행 렌더링 */}
-            {files.map((file, index) => (
-                <tr key={index + 1}>
-                    <th scope="row">
-                        <BsFileEarmark />
-                    </th>
-                    <td>{file.name}</td>
-                    <td>2023.01.01</td>
-                    <td><BiDotsVerticalRounded /></td>
-                </tr>
-            ))}
-            {/* 폴더 행 렌더링 */}
-            {folders.map((folder, index) => (
-                <tr key={index + 1 + files.length}>
-                    <th scope="row">
-                        {/* 폴더 클릭 시 해당 경로 페이지로 이동 */}
-                        <Link to={`/folder/${folder.name}`} className="folder-link" >
-                            <div className="folder-icon">
-                                <BiFolder size="30" />
-                            </div>
-                        </Link>
-                    </th>
-                    <td>
-                        {folder.name}
-                    </td>
-                    <td>2023.01.01</td>
-                    <td><BiDotsVerticalRounded /></td>
-                </tr>
-            ))}
+                {/* 파일 행 렌더링 */}
+                {files.map((file, index) => (
+                    <tr key={index + 1}>
+                        <th scope="row">
+                            <BsFileEarmark />
+                        </th>
+                        <td>{file.name}</td>
+                        <td>2023.01.01</td>
+                        <td><BiDotsVerticalRounded /></td>
+                    </tr>
+                ))}
+                {/* 폴더 행 렌더링 */}
+                {folders.map((folder, index) => (
+                    <tr key={index + 1 + files.length}>
+                        <th scope="row">
+                            {/* 폴더 클릭 시 해당 경로 페이지로 이동 */}
+                            <Link to={`/folder/${folder.name}`} className="folder-link" >
+                                <div className="folder-icon">
+                                    <BiFolder size="30" />
+                                </div>
+                            </Link>
+                        </th>
+                        <td>
+                            {folder.name}
+                        </td>
+                        <td>2023.01.01</td>
+                        <td><BiDotsVerticalRounded /></td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
@@ -78,18 +78,18 @@ const Files = () => {
 
     return (
         <>
-            <NavBar/>
+            <NavBar />
             <div className="filePage">
                 <div className="fileTitle">
                     <h1>Files</h1>
                     <p className="currentLocation">Current Location: {`Files/${folderName}`}</p>
                     {folderName === '' && (
                         <div className="folderCreatorContainer">
-                            <FolderCreator addFolder={addFolder}/>
+                            <FolderCreator addFolder={addFolder} />
                         </div>
                     )}
                 </div>
-                <FileLists folders={folders} files={files}/>
+                <FileLists folders={folders} files={files} />
             </div>
             {/*<FileUploader addFile={addFile}/>*/}
         </>
--- React/khubox/src/components/FolderCreator.js
+++ React/khubox/src/components/FolderCreator.js
@@ -1,82 +1,82 @@
-import React, {useState, useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {BiFolder} from 'react-icons/bi';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { BiFolder } from 'react-icons/bi';
 import '../css/FolderCreator.css';
 
-const FolderCreator = ({addFolder}) => {
-    const [folderName, setFolderName] = useState('');
-    const [folders, setFolders] = useState([]);
-    const [currentFolderPath, setCurrentFolderPath] = useState('/folder');
-    const navigate = useNavigate();
+const FolderCreator = ({ addFolder }) => {
+  const [folderName, setFolderName] = useState('');
+  const [folders, setFolders] = useState([]);
+  const [currentFolderPath, setCurrentFolderPath] = useState('/folder');
+  const navigate = useNavigate();
 
-    const handleInputChange = (e) => {
-        setFolderName(e.target.value);
+  const handleInputChange = (e) => {
+    setFolderName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (folderName.trim() === '') {
+      return;
+    }
+    const newFolder = {
+      id: Date.now(),
+      name: folderName.trim(),
     };
+    // setFolders([...folders, newFolder]);
+    addFolder(newFolder);
+    setFolderName('');
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (folderName.trim() === '') {
-            return;
-        }
-        const newFolder = {
-            id: Date.now(),
-            name: folderName.trim(),
-        };
-        // setFolders([...folders, newFolder]);
-        addFolder(newFolder);
-        setFolderName('');
-    };
+  const handleFolderClick = (folderName) => {
+    setCurrentFolderPath(`${currentFolderPath}/${folderName}`);
+    navigate(`/folder/${folderName}`);
+  };
 
-    const handleFolderClick = (folderName) => {
-      setCurrentFolderPath(`${currentFolderPath}/${folderName}`);
-      navigate(`/folder/${folderName}`);
-    };
-
-    // Render the contents of the current folder
-    const renderFolderContents = () => {
-      const folderPath = currentFolderPath.split('/').slice(1);
-      const currentFolderName = folderPath[folderPath.length - 1];
-      const currentFolder = folders.find((folder) => folder.name === currentFolderName);
-      if (!currentFolder) {
-        return null;
-      }
-
-      return (
-        <div>
-          <h2>Current Folder: {currentFolder.name}</h2>
-          {/* Render the contents of the current folder */}
-        </div>
-      );
-    };
+  // Render the contents of the current folder
+  const renderFolderContents = () => {
+    const folderPath = currentFolderPath.split('/').slice(1);
+    const currentFolderName = folderPath[folderPath.length - 1];
+    const currentFolder = folders.find((folder) => folder.name === currentFolderName);
+    if (!currentFolder) {
+      return null;
+    }
 
     return (
-        <div className="folder-creator">
-            <form className="form-container" onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={folderName}
-                    onChange={handleInputChange}
-                    placeholder="Folder Name"
-                />
-                <button type="submit">Create Folder</button>
-            </form>
-            {/*<div className="folder-list">*/}
-            {/*  {folders.map((folder) => (*/}
-            {/*    <div*/}
-            {/*      key={folder.id}*/}
-            {/*      className="folder-item"*/}
-            {/*      onClick={() => handleFolderClick(folder.name)}*/}
-            {/*    >*/}
-            {/*      <div className="folder-icon">*/}
-            {/*        <BiFolder />*/}
-            {/*      </div>*/}
-            {/*      <div className="folder-name">{folder.name}</div>*/}
-            {/*    </div>*/}
-            {/*  ))}*/}
-            {/*</div>*/}
-            {/*{renderFolderContents()}*/}
-        </div>
+      <div>
+        <h2>Current Folder: {currentFolder.name}</h2>
+        {/* Render the contents of the current folder */}
+      </div>
     );
+  };
+
+  return (
+    <div className="folder-creator">
+      <form className="form-container" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={folderName}
+          onChange={handleInputChange}
+          placeholder="Folder Name"
+        />
+        <button type="submit">Create Folder</button>
+      </form>
+      {/*<div className="folder-list">*/}
+      {/*  {folders.map((folder) => (*/}
+      {/*    <div*/}
+      {/*      key={folder.id}*/}
+      {/*      className="folder-item"*/}
+      {/*      onClick={() => handleFolderClick(folder.name)}*/}
+      {/*    >*/}
+      {/*      <div className="folder-icon">*/}
+      {/*        <BiFolder />*/}
+      {/*      </div>*/}
+      {/*      <div className="folder-name">{folder.name}</div>*/}
+      {/*    </div>*/}
+      {/*  ))}*/}
+      {/*</div>*/}
+      {/*{renderFolderContents()}*/}
+    </div>
+  );
 };
 
 export default FolderCreator;
